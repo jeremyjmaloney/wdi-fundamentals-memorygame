c8fa7e0a246659b25d98c2dfcdfc0398a9b7f4e6
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-03-14 01:27:11 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,14 +26,16 @@ var cardsInPlay = [];
 var checkForMatch = function() {
   if (cardsInPlay.length === 2){
     if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
     } else {
-      console.log("Sorry, try again");
+      alert("Sorry, try again");
     }
   }
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardImage);
   console.log("User flipped a " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
@@ -41,5 +43,15 @@ var flipCard = function(cardId) {
   checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+  for(i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('img');
+    console.log(cardElement);
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+createBoard();
